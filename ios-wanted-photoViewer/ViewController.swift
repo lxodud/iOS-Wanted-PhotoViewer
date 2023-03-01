9c38e685ccb962f143af5c55ce1c73691c9056d8
lxodud <xodud1998223@naver.com>
2023-03-01 17:30:38 +0900

    feat: mainView 구현

--- ios-wanted-photoViewer/ViewController.swift
+++ ios-wanted-photoViewer/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+// MARK: View Configuration
+extension ViewController {
+    private func configureSubview() {
+        
+    }
+}
