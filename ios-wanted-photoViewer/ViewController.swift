0e50ef2d564955914f35feb58ab725a5f4ba9823
lxodud <xodud1998223@naver.com>
2023-03-01 16:02:23 +0900

    initial commit

---
+++ ios-wanted-photoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-wanted-photoViewer
+//
+//  Created by 이태영 on 2023/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
