d50649cad7b5afbf1aecf93fd1bc89fc9be6a612
lxodud <xodud1998223@naver.com>
2023-03-02 21:06:28 +0900

    feat: ImageLoader 클래스 구현

---
+++ ios-wanted-photoViewer/ImageLoader.swift
@@ -0,0 +1,44 @@
+//
+//  ImageLoader.swift
+//  ios-wanted-photoViewer
+//
+//  Created by 이태영 on 2023/03/02.
+//
+
+import UIKit
+
+enum NetworkError: Error {
+    case transportError
+    case serverError
+    case missingData
+}
+
+final class ImageLoader {
+    func loadImage(
+        with url: URL,
+        completionHandler: @escaping (Result<UIImage, NetworkError>) -> Void
+    ) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                completionHandler(.failure(.transportError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode)
+            else {
+                completionHandler(.failure(.serverError))
+                return
+            }
+            
+            guard let data = data,
+                  let image = UIImage(data: data)
+            else {
+                completionHandler(.failure(.missingData))
+                return
+            }
+            
+            completionHandler(.success(image))
+        }.resume()
+    }
+}
--- ios-wanted-photoViewer/ViewController.swift
+++ ios-wanted-photoViewer/ViewController.swift
@@ -8,9 +8,42 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    @IBOutlet var imageViews: [UIImageView]!
+    
+    private let imageURL: [String] = [ "https://wallpaperaccess.com/download/europe-4k-1369012",
+        "https://wallpaperaccess.com/download/europe-4k-1318341",
+        "https://wallpaperaccess.com/download/europe-4k-1379801",
+        "https://wallpaperaccess.com/download/cool-lion-167408",
+        "https://wallpaperaccess.com/download/ironman-hd-323408"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for (index, urlString) in imageURL.enumerated() {
+            let url = URL(string: urlString)!
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard error == nil else { return }
+                
+                guard let response = response as? HTTPURLResponse,
+                      (200...299).contains(response.statusCode)
+                else {
+                    return
+                }
+                
+                guard let data = data,
+                      let image = UIImage(data: data)
+                else {
+                    return
+                }
+                
+                DispatchQueue.main.sync {
+                    self.imageViews[index].image = image
+                }
+            }.resume()
+        }
     }
 }
 
